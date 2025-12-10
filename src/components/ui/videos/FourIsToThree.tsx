21759014c813f65dc53ac8d197af9a6d45279761
josefelipedev <josefelipedev@gmail.com>
2025-12-10 13:26:46 +0000

    first commit

---
+++ src/components/ui/videos/FourIsToThree.tsx
@@ -0,0 +1,17 @@
+
+
+const FourIsToThree = () => {
+    return (
+        <div className="aspect-w-4 aspect-h-3">
+            <iframe
+                src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                className="w-full h-full"
+            ></iframe>
+        </div>
+    );
+}
+
+export default FourIsToThree;
