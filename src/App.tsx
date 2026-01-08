604136fea682dc4da6cf375448409f8a61ab0d01
josefelipedev <josefelipedev@gmail.com>
2026-01-08 09:52:10 +0000

    fix:removing unused files

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,6 @@ import SignIn from './pages/AuthPages/SignIn';
 import SignUp from './pages/AuthPages/SignUp';
 import NotFound from './pages/OtherPage/NotFound';
 import UserProfiles from './pages/UserProfiles';
-import Maintenance from './pages/OtherPage/Maintenance';
-import FiveZeroZero from './pages/OtherPage/FiveZeroZero';
-import FiveZeroThree from './pages/OtherPage/FiveZeroThree';
-import ComingSoon from './pages/OtherPage/ComingSoon';
-import FileManager from './pages/FileManager';
-import Calendar from './pages/Calendar';
-import PricingTables from './pages/PricingTables';
-import Faqs from './pages/Faqs';
-import Blank from './pages/Blank';
 import ResetPassword from './pages/AuthPages/ResetPassword';
 import TwoStepVerification from './pages/AuthPages/TwoStepVerification';
 import Success from './pages/OtherPage/Success';
@@ -35,8 +26,6 @@ export default function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/two-step-verification" element={<TwoStepVerification />} />
-            <Route path="/maintenance" element={<Maintenance />} />
-            <Route path="/coming-soon" element={<ComingSoon />} />
           </Route>
 
           {/* Rotas Protegidas (requerem autenticação) */}
@@ -46,19 +35,12 @@ export default function App() {
 
               {/* Others Page */}
               <Route path="/profile" element={<UserProfiles />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/faq" element={<Faqs />} />
-              <Route path="/pricing-tables" element={<PricingTables />} />
-              <Route path="/blank" element={<Blank />} />
-              <Route path="/file-manager" element={<FileManager />} />
             </Route>
           </Route>
 
           {/* Rotas de Erro (acessíveis por todos) */}
           <Route path="*" element={<NotFound />} />
           <Route path="/success" element={<Success />} />
-          <Route path="/five-zero-zero" element={<FiveZeroZero />} />
-          <Route path="/five-zero-three" element={<FiveZeroThree />} />
         </Routes>
       </AuthChecker>
     </Router>
